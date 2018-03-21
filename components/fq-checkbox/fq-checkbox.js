c6db31fb687d3aa17dedacd6087815c4f228ea25
smilelight <1459679436@qq.com>
2018-03-21 15:49:31 +0800

    增加四象限测试

---
+++ components/fq-checkbox/fq-checkbox.js
@@ -0,0 +1,33 @@
+// components/fq-checkbox/fq-checkbox.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  ready() {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleToggle(e) {
+      this.data.checked = !this.data.checked
+      this.setData(this.data)
+      this.triggerEvent('change', this)
+    }
+  }
+})
--- pages/fqTodoItemTest/fqTodoItemTest.js
+++ pages/fqTodoItemTest/fqTodoItemTest.js
@@ -1,10 +1,25 @@
 // pages/fqTodoItemTest/fqTodoItemTest.js
+import Todo from '../../models/Todo'
+import todoStore from '../../store/todoStore'
+
+//获取应用实例
+const app = getApp
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    // todos
+    todos: [],
+
+    // todo 计数
+    uncompletedCount: 0,
+    completedCount: 0,
+
+    // 是否动画延迟
+    delay: true
   
   },
 
@@ -26,14 +41,14 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.syncData();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    this.syncData();
   },
 
   /**
@@ -62,5 +77,28 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  syncData() {
+    // 获取列表
+    this.data.todos = todoStore.getTodos()
+    this.data.todos.forEach(todo => console.log(todo.level));
+    this.update()
+    // 更新置顶标题
+    let uncompletedCount = todoStore.getUncompletedTodos().length
+    let todayCompletedCount = todoStore.getTodayCompletedTodos().length
+    let title = ['TodoList（进行中: ', uncompletedCount, ', 今日已完成: ', todayCompletedCount, '）'].join('')
+    wx.setTopBarText({ text: title })
+    // 动画结束后取消动画队列延迟
+    setTimeout(() => {
+      this.update({ delay: false })
+    }, 2000)
+  },
+
+  update(data) {
+    data = data || this.data
+    data.completedCount = todoStore.getCompletedTodos().length
+    data.uncompletedCount = todoStore.getUncompletedTodos().length
+    this.setData(data)
+  },
 })
